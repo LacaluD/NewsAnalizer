9d2d9937f947590d37659fa149abb45282986bba
Artem <204591738+LacaluD@users.noreply.github.com>
2026-06-04 12:35:31 +0200

    Refactor TelegramBot command handling and update versioning logic + Refactor version.py to improve readability of versioning logic

--- version.py
+++ version.py
@@ -3,14 +3,22 @@ import subprocess
 __version__ = "1.0.0"
 
 try:
-    __build__ = subprocess.check_output(
-        ["git", "rev-list", "--count", "HEAD"],
-        stderr=subprocess.DEVNULL,
-    ).decode().strip()
-    __commit__ = subprocess.check_output(
-        ["git", "rev-parse", "--short", "HEAD"],
-        stderr=subprocess.DEVNULL,
-    ).decode().strip()
+    __build__ = (
+        subprocess.check_output(
+            ["git", "rev-list", "--count", "HEAD"],
+            stderr=subprocess.DEVNULL,
+        )
+        .decode()
+        .strip()
+    )
+    __commit__ = (
+        subprocess.check_output(
+            ["git", "rev-parse", "--short", "HEAD"],
+            stderr=subprocess.DEVNULL,
+        )
+        .decode()
+        .strip()
+    )
 except Exception:
     __build__ = "__dev__"
     __commit__ = "__dev__"
